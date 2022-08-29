2a5ca476b35d36b585dba780d77462daf60f7920
Yura <yuraborisov19@gmail.com>
2022-08-29 17:32:56 +0300

    updated #29

--- src/pages/notfoundPage/index.tsx
+++ src/pages/notfoundPage/index.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 import { NotFoundPageWrapper, NotFoundPageContent } from "./styles";
 
-export const NotFoundPage = () => {
-  return <NotFoundPageWrapper>
+export const NotFoundPage = () => (
+  <NotFoundPageWrapper>
     <NotFoundPageContent>
       <p>not-found</p>
       <Link to="/">Home</Link>
     </NotFoundPageContent>
   </NotFoundPageWrapper>
-}
+);
